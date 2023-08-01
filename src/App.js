316a2516deefe93d19d91da53789441ffdf11e74
goosefin <agustinafin96@gmail.com>
2023-07-31 20:47:33 -0400

    cleaned up file structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Todo from "./components/Todo";
-import InlineStyle from "./components/InlineStyle";
 
 function App() {
   return (
     <div>
-      {/* <Todo/> */}
-      <InlineStyle/>
+      <Todo/>
     </div>
   );
 }
